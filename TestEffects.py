cc2bd3b3e0d389abde4be663b69441da62b6b35c
mcolinj <mcolinj@mac.com>
2025-07-17 23:53:05 -0400

    Update TestEffects.py
    Move slowness to the effect constructor so the slowness can be defined at the proper time.
    
    Change the Effects Chooser to return 3 effects running at different speeds.

--- TestEffects.py
+++ TestEffects.py
@@ -21,6 +21,7 @@ PURPLE = (92, 50, 168)
 ORANGE = (235, 122, 52)
 BLUE = (24, 30, 214)
 BUTTERSCOTCH = (252, 186, 3)
+GREEN = (3, 252, 92)
 
 class EffectChooser:
     
@@ -39,15 +40,15 @@ class EffectChooser:
         Return a list of effects
         """
         return [
-                   WipeFillEffect(pixels, 0, 8, color=BLUE),
-                   WipeFillEffect(pixels, 8, 8, color=BUTTERSCOTCH),
-                   WipeFillEffect(pixels, -8, 8, color=ORANGE)
+                   WipeFillEffect(pixels, 0, 32, color=PURPLE, slowness=2),
+                   WipeFillEffect(pixels, 0, 32, color=ORANGE, slowness=4),
+                   WipeFillEffect(pixels, 0, 32, color=GREEN, slowness=8)
         ]
 
 
 class WipeFillEffect:
 
-    def __init__(self, pixels, start_index, num_pixels, color=PURPLE, brightness=BRIGHTNESS, clear_on_init=True):
+    def __init__(self, pixels, start_index, num_pixels, color=PURPLE, slowness=2, brightness=BRIGHTNESS, clear_on_init=True):
         """
         Create a lighting effect that fills PIXELS in the specified range, with the specified color.
         Defaults are all PURPLE pixels at the global BRIGHTNESS.
@@ -57,6 +58,7 @@ class WipeFillEffect:
         self._start_index = start_index
         self._num_pixels = num_pixels
         self._color = color
+        self._slowness = slowness
         self._brightness = brightness
 
         if clear_on_init:
@@ -65,7 +67,7 @@ class WipeFillEffect:
             """
             pixels.fill(OFF)
 
-    def make_generator(self, slowness=10):
+    def make_generator(self):
         """
         Need to decide how to set the brightness separately
         maybe just scale the self._color on __init__?
@@ -79,7 +81,7 @@ class WipeFillEffect:
             The yield command here, turns this function into a generator, so it returns after each
             iteration of the loop.   Subsequent calls pick up where they left off.
             """
-            for _ in range(slowness):
+            for _ in range(self._slowness):
                 yield
 
 
@@ -97,6 +99,10 @@ if __name__ == "__main__":
 
     while len(do_effects) > 0 or len(next_do_effects) > 0:
         try:
+            """
+            Remove the effect from the front of the list.  If it runs without stopping,
+            put it at the end of the list to be used on the next pass through the effects
+            """
             do_effect = do_effects.pop(0)
             try:
                 next(do_effect)
@@ -104,10 +110,10 @@ if __name__ == "__main__":
             except StopIteration:
                 print("Generator stopped: ", do_effect)
         except IndexError:
-            # restore the list for the next set of runs
+            # at the end of the list
+            # restore it with the list for the next pass
             do_effects = next_do_effects
             next_do_effects = []
             
         pixels.show()
         time.sleep(0.02)
-

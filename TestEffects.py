fcad6e6f3f5001c6cb6967a26eb2b26e4c68ac32
mcolinj <mcolinj@mac.com>
2025-07-18 09:23:10 -0400

    Update TestEffects.py
    Fix the Orchestrator loop (the one that runs the effects) so that the sleep is in the outer loop.    Things were running way too slowly, and this change puts it in the right place.     The example Wipe effect runs at 50Hz cleanly now.

--- TestEffects.py
+++ TestEffects.py
@@ -22,27 +22,29 @@ ORANGE = (235, 122, 52)
 BLUE = (24, 30, 214)
 BUTTERSCOTCH = (252, 186, 3)
 GREEN = (3, 252, 92)
+PINK = (248, 3, 252)
 
 class EffectChooser:
-    
+
     def __init__(self, pixels):
         self._pixels = pixels
         pass
-        
+
     def get_chosen_effect(self):
         """
         Currently there is only one effect
         """
         return WipeFillEffect(pixels, 0, 8)
-        
+
     def get_chosen_effects(self):
         """
         Return a list of effects
         """
         return [
-                   WipeFillEffect(pixels, 0, 32, color=PURPLE, slowness=2),
-                   WipeFillEffect(pixels, 0, 32, color=ORANGE, slowness=4),
-                   WipeFillEffect(pixels, 0, 32, color=GREEN, slowness=8)
+                   WipeFillEffect(pixels, 0, 8, color=PURPLE, slowness=1),
+                   WipeFillEffect(pixels, 8, 8, color=ORANGE, slowness=1),
+                   WipeFillEffect(pixels, 16, 8, color=PINK, slowness=1),
+                   WipeFillEffect(pixels, 24, 8, color=GREEN, slowness=1)
         ]
 
 
@@ -52,7 +54,7 @@ class WipeFillEffect:
         """
         Create a lighting effect that fills PIXELS in the specified range, with the specified color.
         Defaults are all PURPLE pixels at the global BRIGHTNESS.
-        The filling is done at 50hz using a python generator to break up the updates
+        The filling is done at 20hz using a python generator to break up the updates
         """
         self._pixels = pixels
         self._start_index = start_index
@@ -91,29 +93,34 @@ if __name__ == "__main__":
     pixels = neopixel.NeoPixel(board.GP6, NUM_PIXELS, brightness = BRIGHTNESS, auto_write = False)
     pixels.fill((0,0,0))
     chooser = EffectChooser(pixels)
-    
+
     effects = chooser.get_chosen_effects()
     do_effects = [ effect.make_generator() for effect in effects ]
-    print(do_effects)
     next_do_effects = []
 
     while len(do_effects) > 0 or len(next_do_effects) > 0:
-        try:
-            """
-            Remove the effect from the front of the list.  If it runs without stopping,
-            put it at the end of the list to be used on the next pass through the effects
-            """
-            do_effect = do_effects.pop(0)
-            try:
-                next(do_effect)
-                next_do_effects.append(do_effect)
-            except StopIteration:
-                print("Generator stopped: ", do_effect)
-        except IndexError:
-            # at the end of the list
-            # restore it with the list for the next pass
-            do_effects = next_do_effects
-            next_do_effects = []
+        
+        all_live_effects_have_run_once = False
+        
+        while not all_live_effects_have_run_once:
             
+            try:
+                """
+                Remove the effect from the front of the list.  If it runs without stopping,
+                put it at the end of the list to be used on the next pass through the effects
+                """
+                do_effect = do_effects.pop(0)
+                try:
+                    next(do_effect)
+                    next_do_effects.append(do_effect)
+                except StopIteration:
+                    pass
+            except IndexError:
+                # at the end of the list
+                # restore it with the list for the next pass
+                do_effects = next_do_effects
+                next_do_effects = []
+                all_live_effects_have_run_once = True
+
         pixels.show()
         time.sleep(0.02)

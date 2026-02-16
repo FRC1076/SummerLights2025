fbe50b8de2462b5ffb7a14b41f1fe156deae8b7b
Matthew Jones <mcolinj@mac.com>
2026-02-15 22:17:30 -0500

    Add tools.  Clean up some files

---
+++ pull.sh
@@ -0,0 +1,15 @@
+# This command copies all of the files (whether or not they have been changed),
+# from the CIRCUITPY disk into the current directory.    This makes it easier
+# for you to make sure that all of the changes that you made on the CIRCUITPY
+# disk get copied into your git development directory so you can commit and push
+# the code.
+#
+# There are a few other ways this could be done.
+# Could use the rsync command, which does comparisons based on timestamps on the
+# file which could prevent you from clobbering something that you had changed
+# in the development directory.
+# Could maybe create your code repository *on* the CIRCUITPY disk, so you don't
+# have to worry at all about copying back and forth.    My concern there is just
+# whether or not the contents of the could get corrupted, as a consequence lose
+# more code.   (I haven't thought about this much...)
+cp /Volumes/CIRCUITPY/code.py /Volumes/CIRCUITPY/Compositor.py /Volumes/CIRCUITPY/LightingEffects.py /Volumes/CIRCUITPY/NeoConfig.py /Volumes/CIRCUITPY/Physics.py /Volumes/CIRCUITPY/PixelBuffer.py /Volumes/CIRCUITPY/TapDetector.py /Volumes/CIRCUITPY/SoundDetector.py .
---
+++ push.sh
@@ -0,0 +1,1 @@
+cp code.py Compositor.py LightingEffects.py NeoConfig.py Physics.py PixelBuffer.py TapDetector.py SoundDetector.py /Volumes/CIRCUITPY

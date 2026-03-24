af5355a8d9106c3f37fc56883c73622a8d514eae
Matthew Jones <mcolinj@mac.com>
2026-03-23 22:46:42 -0400

    Update to add HardwareAwareness.py

--- copy_to_device.py
+++ copy_to_device.py
@@ -1,1 +1,2 @@
-cp code.py Compositor.py FlipFlopEffect.py LightingEffects.py NeoConfig.py Physics.py PixelBuffer.py SoundDetector.py TapDetector.py TestAnEffect.py ButtonChooser.py ControlEffects.py EffectChooser.py DemoCommands.py /Volumes/CIRCUITPY
+cp code.py Compositor.py FlipFlopEffect.py LightingEffects.py NeoConfig.py Physics.py PixelBuffer.py SoundDetector.py TapDetector.py TestAnEffect.py ButtonChooser.py ControlEffects.py EffectChooser.py DemoCommands.py HardwareAwareness.py /Volumes/CIRCUITPY
+
